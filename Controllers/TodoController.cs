22f904eaf13b8aaa76aa4389df277689794740ee
OkuderaYuki <>
2018-08-22 01:21:58 +0900

    [Add] ToDoアイテムを取得するHttpGetメソッドを実装しました

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -7,6 +7,10 @@ namespace TodoApi.Controllers
 {
 
     // [Route("api/[controller]")]属性: コントローラやアクション単位でルーティングを記述する
+    // 
+    // [controller]は、"Controller"サフィックスを除いたクラス名に置き換えられる
+    // ASP.NET Core のルーティングでは、大文字と小文字が区別されない
+    // 本クラスの場合、"todo"となる
     [Route("api/[controller]")]
     // [ApiController]属性: WebAPIコントローラークラスを表す
     // ControllerBaseと組み合わせて使用する
@@ -28,6 +32,23 @@ namespace TodoApi.Controllers
                 _context.TodoItems.Add(new TodoItem { Name = "Item1" });
                 _context.SaveChanges();
             }
+        }
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            // MVCは、自動的にオブジェクトをJSONにシリアライズして、レスポンスメッセージに書き込む
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<TodoItem> GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
         }
     }
 }
